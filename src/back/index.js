6edfb0596e73d4168f8b047b81fa6440b2aa8c97
jairo25fdez <jairo25fdez@gmail.com>
2020-06-20 20:51:00 +0200

    Added index.js in backend

---
+++ src/back/index.js
@@ -0,0 +1,27 @@
+module.exports = function(app){
+
+    const MongoClient = require('mongodb').MongoClient;
+    const assert = require('assert');
+
+    // Connection URL
+    const url = 'mongodb://localhost:27017';
+
+    // Database Name
+    const dbName = 'database';
+
+    // Create a new MongoClient
+    const client = new MongoClient(url);
+
+    // Use connect method to connect to the Server
+    client.connect(function(err) {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+
+        const db = client.db(dbName);
+
+        client.close();
+    });
+
+
+
+}
