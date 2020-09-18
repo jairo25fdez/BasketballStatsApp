61f25f26dd6a04cb454fa98acbd6c3bacdee383f
jairo25fdez <jairo25fdez@gmail.com>
2020-09-18 19:32:49 +0200

    Added app.get to index

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ app.use(bodyParser.json());
 
 const port = process.env.PORT || 8000;
 //app.use("/", express.static(path.join(__dirname,"/front/myteam-app/src")));
+
+//Prueba Heroku
 app.use("/", express.static(path.join(__dirname,"/front/myteam-app/dist/myteam-app")));
+app.get("/*", (req, res) => res.sendFile(path.join(__dirname)));
 
 const stats_API = require(path.join(__dirname,"/back"));
 stats_API(app);
