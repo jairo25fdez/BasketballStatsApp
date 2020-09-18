d60bb6213d50538a9e75a13ce93e6c48b112ed75
jairo25fdez <jairo25fdez@gmail.com>
2020-09-18 19:21:52 +0200

    Fixed Angular installation

--- front/myteam-app/src/app/app.component.spec.ts
+++ front/myteam-app/src/app/app.component.spec.ts
@@ -1,13 +1,9 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
         AppComponent
       ],
@@ -20,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'myteam'`, () => {
+  it(`should have as title 'myteam-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('myteam');
+    expect(app.title).toEqual('myteam-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('myteam app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('myteam-app app is running!');
   });
 });
--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 8000;
-app.use("/", express.static("./front/myteam/src"));
+//app.use("/", express.static(path.join(__dirname,"/front/myteam-app/src")));
+app.use("/", express.static(path.join(__dirname,"/front/myteam-app/dist/myteam-app")));
 
 const stats_API = require(path.join(__dirname,"/back"));
 stats_API(app);
