998e1d152344565c3a7c3bf8272b54efa1d046bb
jairo25fdez <jairo25fdez@gmail.com>
2020-09-21 21:14:46 +0200

    Commented enironment.prod apiUrl line

--- front/myteam/src/environments/environment.prod.ts
+++ front/myteam/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://jbasketstats.herokuapp.com/api" //API's domain, Heroku doesnt allows u to "open" multiple ports.
+  //apiUrl: "https://jbasketstats.herokuapp.com/api" //API's domain, Heroku doesnt allows u to "open" multiple ports.
 };
