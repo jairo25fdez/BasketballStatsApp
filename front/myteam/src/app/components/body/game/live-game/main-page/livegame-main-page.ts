fc29c6b40e41604f57205b9c00f7586d1055d617
jairo25fdez <jairo25fdez@gmail.com>
2020-10-21 02:39:19 +0200

    createStealPlay updated

--- front/myteam/src/app/components/body/game/live-game/main-page/livegame-main-page.ts
+++ front/myteam/src/app/components/body/game/live-game/main-page/livegame-main-page.ts
@@ -722,7 +722,33 @@ export class MainPageComponent implements OnInit {
       play.type = "steal";
 
       //Post the play
-      this.playsService.createPlay(play).catch( (err:HttpErrorResponse) => {
+      this.playsService.createPlay(play).then( () => {
+
+        //If the player belongs to the home team
+        if(this.player_active[0] == 0){
+
+          this.home_players[this.player_active[1]].steals++;
+          this.home_team_stats.steals++;
+          
+          this.player_stats_gameService.updatePlayer_stats_game(this.home_players[this.player_active[1]]);
+
+          this.team_stats_gameService.updateTeam_stats_game(this.home_team_stats);
+          
+        }
+        //If the player belongs to the visitor team
+        else{
+
+          this.visitor_players[this.player_active[1]].steals++;
+          this.visitor_team_stats.steals++;
+
+          this.player_stats_gameService.updatePlayer_stats_game(this.visitor_players[this.player_active[1]]);
+
+          this.team_stats_gameService.updateTeam_stats_game(this.visitor_team_stats);
+
+        }
+
+      })
+      .catch( (err:HttpErrorResponse) => {
         Swal.fire({
           title: 'Error al crear la jugada.',
           icon: 'error'
